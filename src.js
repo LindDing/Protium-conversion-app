9b2181f4229561d0b4987abb9de5d94c845d1c0b
LindDing <90715872+LindDing@users.noreply.github.com>
2022-01-10 13:45:16 +0100

    buttons

---
+++ src.js
@@ -0,0 +1,33 @@
+
+function myFunction1() {
+    var x = document.getElementById("section1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+function myFunction2() {
+    var x = document.getElementById("section2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+function myFunction3() {
+    var x = document.getElementById("section3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+function myFunction4() {
+    var x = document.getElementById("section4");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
